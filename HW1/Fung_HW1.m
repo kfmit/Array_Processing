4f223eeaaf8a133393b030a0c4f7a29419539258
kfmit <kfung@mit.edu>
2021-10-19 22:53:50 -0400

    finished hw1

--- HW1/Fung_HW1.m
+++ HW1/Fung_HW1.m
@@ -11,9 +11,7 @@ clc
 % Problem 1.3: see document
 
 % Problem 2: Creating a 7 element array
-N = 7;  % Number of elements
-lambda= 2; % arbitrary wavelength for fun
-D = lambda/2; % Element spacing (m)
+%D = lambda/2; % Element spacing (m)
 phi_s = 90;    % steering angle
 theta_0 = 90; % broadside, coming in
 
@@ -24,11 +22,11 @@ theta_0 = 90; % broadside, coming in
 
 fc = 1e9;   % i can make this whatever i want
 % uniform weighting
-w = [ 1/7 1/7 1/7 1/7 1/7 1/7 1/7]; % all on
-% w = [ 0 1/7 1/7 1/7 1/7 1/7 1/7]; % first out
-% w = [ 1/7 1/7 0 1/7 1/7 1/7 1/7]; % third out
-% w = [ 1/7 1/7 0 0 1/7 0 1/7]; % 3/4/6 out
-% w = [ 1/7 0 1/7 0 1/7 0 1/7]; % 2/4/6 out
+w1 = [ 1/7 1/7 1/7 1/7 1/7 1/7 1/7]; % all on
+w2 = [ 0 1/7 1/7 1/7 1/7 1/7 1/7]; % first out
+w3 = [ 1/7 1/7 0 1/7 1/7 1/7 1/7]; % third out
+w4 = [ 1/7 1/7 0 0 1/7 0 1/7]; % 3/4/6 out
+w5 = [ 1/7 0 1/7 0 1/7 0 1/7]; % 2/4/6 out
 
 %c = 1525;   % water soundspeed, why not
 % resp = ula(fc,ang)    % some built in function
@@ -38,6 +36,7 @@ w = [ 1/7 1/7 1/7 1/7 1/7 1/7 1/7]; % all on
 N=7;                        % Number of elements
 D=[-(N-1)/2:1:(N-1)/2].';   % Distance, column vector
 %w = [ones(N,1)]/N;          % weight of 1/N (1/7), unifrom
+n = [1:1:N];
 
 theta = [0:0.01:pi];        % vecotr of thetas from 0 to pi
 psi = [-1:0.01:1]*pi;       % vector of psis from -pi to pi
@@ -46,38 +45,167 @@ v_psi = exp(1i*D*psi);           % vector of vs e^j*D*psi
 % CHECK ON THIS
 v_theta = exp(1i*D*cos(theta));           % vector of vs e^j*D*theta DO I NEED COS OR NOT
 
-B_psi = w*v_psi;                   % weights times vector v
-B_theta = w*v_theta;
-B_theta = 10*log10(abs(B_theta).^2);
-B_psi = 10*log10(abs(B_psi).^2);
+B1_psi = w1*v_psi;                   % weights times vector v
+B1_theta = w1*v_theta;
+B2_psi = w2*v_psi;                   % weights times vector v
+B2_theta = w2*v_theta;
+B3_psi = w3*v_psi;                   % weights times vector v
+B3_theta = w3*v_theta;
+B4_psi = w4*v_psi;                   % weights times vector v
+B4_theta = w4*v_theta;
+B5_psi = w5*v_psi;                   % weights times vector v
+B5_theta = w5*v_theta;
 
+% B_theta = 10*log10(abs(B_theta).^2); % turn to power
+% B_psi = 10*log10(abs(B_psi).^2);
+%Normalize
+B1n_psi = max(abs(B1_psi))./abs(B1_psi);
+B2n_psi = max(abs(B2_psi))./abs(B2_psi);
+B3n_psi = max(abs(B3_psi))./abs(B3_psi);
+B4n_psi = max(abs(B4_psi))./abs(B4_psi);
+B5n_psi = max(abs(B5_psi))./abs(B5_psi);
+
+B1n_theta = max(abs(B1_theta))./abs(B1_theta);
+B2n_theta = max(abs(B2_theta))./abs(B2_theta);
+B3n_theta = max(abs(B3_theta))./abs(B3_theta);
+B4n_theta = max(abs(B4_theta))./abs(B4_theta);
+B5n_theta = max(abs(B5_theta))./abs(B5_theta);
 %%
 figure(2);
 % This plots the instantaneous power for every element (N waveforms).
 subplot(2,1,1)
-plot(theta,normalize(B_theta,'range'));
- ylim([0 1.5]);
+% plot(theta,normalize(B_theta,'range'));
+plot(theta,10*log10(abs(B1_theta).^2));
+%  ylim([0 1.5]);
 %xlim([0 pi]);
 grid on;
-title('Beampattern in \Theta');
+title('Normalized Beampattern in \Theta');
 xlabel('Theta');
 ylabel('Output Power (dB)');
 %
 % hold on;
-subplot(2,1,2)
-plot(psi,normalize(B_psi,'range'),'r');
+ subplot(2,1,2)
+% plot(psi,normalize(B_psi,'range'),'r');
+plot(psi,10*log10(abs(B1_psi).^2));
 grid on;
-ylim([0 1.5]);
+%ylim([0 1.5]);
 %xlim([-90 90]);
-title('Beampattern in \Psi');
+title('Normalized Beampattern in \Psi');
 xlabel('Psi');
 ylabel('Output Power (dB)');
 %tightfig;
 
 
+%% Making one figure to rule them all
 
+figure(3)
+subplot(2,1,1)
+% plot(theta,normalize(B_theta,'range'));
+hold on
+plot(theta,10*log10(abs(B1_theta).^2));
+plot(theta,10*log10(abs(B2_theta).^2));
+plot(theta,10*log10(abs(B3_theta).^2));
+plot(theta,10*log10(abs(B4_theta).^2));
+plot(theta,10*log10(abs(B5_theta).^2));
+hold off
+%ylim([0 1.5]);
+%xlim([0 pi]);
+grid on;
+title('Beampattern in \Theta');
+xlabel('Theta');
+ylabel('Output Power (dB)');
+legend('All on','1 out','3 out','3/4/6 out','2/4/6 out')
+%
+% hold on;
+subplot(2,1,2)
+% plot(psi,normalize(B_psi,'range'),'r');
+hold on
+plot(psi,10*log10(abs(B1_psi).^2));
+plot(psi,10*log10(abs(B2_psi).^2));
+plot(psi,10*log10(abs(B3_psi).^2));
+plot(psi,10*log10(abs(B4_psi).^2));
+plot(psi,10*log10(abs(B5_psi).^2));
+hold off
+grid on;
+%ylim([0 1.5]);
+%xlim([-90 90]);
+title('Beampattern in \Psi');
+xlabel('Psi');
+ylabel('Output Power (dB)');
+legend('All on','1 out','3 out','3/4/6 out','2/4/6 out')
 
+%%
+figure(4)
+subplot(2,1,1)
+% plot(theta,normalize(B_theta,'range'));
+hold on
+plot(theta,abs(B1_theta));
+plot(theta,abs(B2_theta));
+plot(theta,abs(B3_theta));
+plot(theta,abs(B4_theta));
+plot(theta,abs(B5_theta));
+hold off
+%ylim([0 1.5]);
+%xlim([0 pi]);
+grid on;
+title('Normalized Beampattern in \Theta');
+xlabel('Theta');
+ylabel('Output Amplitude Normalized');
+legend('All on','1 out','3 out','3/4/6 out','2/4/6 out')
+%
+% hold on;
+subplot(2,1,2)
+% plot(psi,normalize(B_psi,'range'),'r');
+hold on
+plot(psi,abs(B1_psi));
+plot(psi,abs(B2_psi));
+plot(psi,abs(B3_psi));
+plot(psi,abs(B4_psi));
+plot(psi,abs(B5_psi));
+hold off
+grid on;
+%ylim([0 1.5]);
+%xlim([-90 90]);
+title('Normalized Beampattern in \Psi');
+xlabel('Psi');
+ylabel('Output Amplitude Normalized');
+legend('All on','1 out','3 out','3/4/6 out','2/4/6 out')
 
-
+%%
+figure(5)
+subplot(2,1,1)
+% plot(theta,normalize(B_theta,'range'));
+hold on
+plot(theta,B1_theta);
+plot(theta,B2_theta);
+plot(theta,B3_theta);
+plot(theta,B4_theta);
+plot(theta,B5_theta);
+hold off
+%ylim([0 1.5]);
+%xlim([0 pi]);
+grid on;
+title('Raw Beampattern in \Theta');
+xlabel('Theta');
+ylabel('Output Amplitude Raw');
+legend('All on','1 out','3 out','3/4/6 out','2/4/6 out')
+%
+% hold on;
+subplot(2,1,2)
+% plot(psi,normalize(B_psi,'range'),'r');
+hold on
+plot(psi,B1_psi);
+plot(psi,B2_psi);
+plot(psi,B3_psi);
+plot(psi,B4_psi);
+plot(psi,B5_psi);
+hold off
+grid on;
+%ylim([0 1.5]);
+%xlim([-90 90]);
+title('Raw Beampattern in \Psi');
+xlabel('Psi');
+ylabel('Output Amplitude Raw');
+legend('All on','1 out','3 out','3/4/6 out','2/4/6 out')
 
 
--- HW1/Prob_2_7_1.m
+++ HW1/Prob_2_7_1.m
@@ -1,45 +1,80 @@
 %% Problem 4: Van Trees 2.7.1
-% Note in part (a) the aperture is continuous. In parts (b) and © plot the respective beampatterns (magnitude). In part (c) the equivalent array shoulda also have triangular weighting. Plot the beampatterns out to include the 3rd sidelobe away from the mainlobe on each side in each part: Consider a linear aperture with L = 5ƛ. Assume that the weighting function in triangular (peak in middle)
+% Note in part (a) the aperture is continuous. In parts (b) and © plot the respective beampatterns (magnitude). 
+%In part (c) the equivalent array shoulda also have triangular weighting. Plot the beampatterns out to include
+%the 3rd sidelobe away from the mainlobe on each side in each part: 
+%Consider a linear aperture with L = 5ƛ. Assume that the weighting function in triangular (peak in middle)
 % Find an expression for the beam pattern and plot 
 
 clear all;
 close all;
 clc
 
-% with seven elements
-N=7;                        % Number of elements
-D=[-(N-1)/2:1:(N-1)/2].';   % Distance, column vector
-w = [ones(N,1)]/N;          % weight of 1/N (1/7), unifrom
-w_tri=triang(N);
-
-psi = [-1:0.01:1]*pi;       % vector of psis from -pi to pi
-v = exp(j*D*psi);           % ve cot of vs e^j*D*psi
-
-B = w'*v;                   % weights times vector v
-
-D4 = [-3 -2 1 3]';          % messing with distances, not equal in length
-w4 = ones(4,1)/4;           % 
-v4 = exp(j*D4*psi);
-
-B4 = w4'*v4;
+% Plotting the triangle beampattern
+lambda = 1;     % amke it something so he's not lonely
+L = 5*lambda;    % definition as given by the problem
+u = [-1:0.01:1]; % want to be able to see enough
+B_tri = (sinc(5*pi*u)).^2;
+% B_tri = 10*log(abs(B_tri.^2));
 
 figure(1)
-h1 =plot(psi/pi, 10*log10(abs(B4).^2),'-');
+plot(u,B_tri)
+xlabel('u')
+ylabel('Output Amplitude')
+title('Beampattern of triangle')
+%% How is the beam pattern in part (a) related to the beam pattern for uniform weighing:
+B_uni = sinc(5*pi*u);
+
+figure(2)
+plot(u,B_tri)
+hold on
+plot(u,B_uni)
+xlabel('u')
+ylabel('Output Amplitude')
+title('Beampattern of trianglular and uniform')
+legend('Triangular','Uniform')
+
+%%
+% with seven elements
+N=40;                        % Number of elements, i canchange
+D2=[-(N-1)/2:1:(N-1)/2];   % Distance, column vector
+D4=[-(N-1)/4:1:(N-1)/4];   % Distance, column vector
+w2 = [ones(40,1)]/40;          % weight of 1/N (1/7), unifrom
+w4 = [ones(20,1)]/20;          % weight of 1/N (1/7), unifrom
+
+v2 = exp(1i*D2'*u);           % vector of vs e^j*D*psi
+v4 = exp(1i*D4'*u);           % vector of vs e^j*D*psi
+
+
+B_4 = w4'*v4;
+B_2 = w2'*v2;
+
+
+figure(1)
+h1 =plot(u, 10*log10(abs(B_tri).^2),'-');
+hold on
+h2 =plot(u, 10*log10(abs(B_4).^2),'-');
+hold on
+h3 =plot(u, 10*log10(abs(B_2).^2),'-');
 grid on
 ylabel('Beampattern(dB)')
-title('Nonuniform 4 Element Beampattern')
-xlabel('psi/pi')
+title('Triangular and Uniform Weighting')
+xlabel('u')
+legend([h1 h2 h3],'Triangular','d=\Lambda/4','d =\Lambda/2')
 
-%% Finding Beamwidth nullnull
-u = [0.2:0.0001:0.3];   % u=cos(theta)
-vv = exp(j*D4*pi*u);
-B = w4'*vv;
-[y,I]=min(abs(B));
-umin = u(I)
-BWmm_u = 2*umin
-psimin = pi*umin
-BWmm_psi = 2*psimin
-Bmin = y
+%%
+figure(3)
+h1 =plot(u, abs(B_tri),'-');
+hold on
+h2 =plot(u, abs(B_4),'-');
+hold on
+h3 =plot(u, abs(B_2),'-');
+grid on
+ylabel('Beampattern abs(B)')
+title('Triangular and Uniform Weighting')
+xlabel('u')
+legend([h1 h2 h3],'Triangular','d=\Lambda/4','d =\Lambda/2')
+
+%%
 
 figure(2)
 h1 =plot(psi/pi, 10*log10(abs(B4).^2),'-');
@@ -59,3 +94,24 @@ hold off
 
 % How is the beam pattern in part (a) related to the beam pattern for uniform weighing
 % Compare the result in part (a) to an equivalent linear array with d=/4 and d=/2
+N = 20;
+w = (1/N)*ones(N,1);
+d = [-(N-1)/2:1:(N-1)/2];
+v_u10 = exp(1i*d'*pi/2*u);
+
+N2 = 10;
+w2 = (1/N2)*ones(N2,1);
+d20 = [-(N2-1)/2:1:(N2-1)/2];
+v_u20 = exp(1i*d20'*pi*u);
+
+BW_10u = w'*v_u10;
+BW_20u = w2'*v_u20;
+
+figure
+plot(u,BW_10u)
+grid
+hold on
+plot(u,BW_20u)
+plot(u,BWw)
+xlabel('u')
+ylabel('BW')
